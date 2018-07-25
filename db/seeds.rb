d11b956f52a7128c9302e3df82720b1ca9acd6b9
8titi8 <raymathild@gmail.com>
2018-07-26 01:49:50 +0200

    ajout seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
+
+50.times do
+	eleve = Eleve.create(first_name: Faker::Name.first_name, 
+						last_name: Faker::Name.last_name,
+						email: Faker::Internet.free_email,
+						age: rand(18..45), cour_id: rand(1..5)) 
+end
+
+10.times do
+	cour = Cour.create(title: Faker::HowIMetYourMother.catch_phrase,
+						description: Faker::HowIMetYourMother.quote)
+end
